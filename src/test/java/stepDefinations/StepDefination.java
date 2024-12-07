1900dae7a6c5e0b785fe849a047d7bc649fc1b73
Akshay <akshaykhonde2021@gmail.com>
2024-12-07 17:14:03 +0530

    first commit

---
+++ src/test/java/stepDefinations/StepDefination.java
@@ -0,0 +1,32 @@
+package stepDefinations;
+
+import io.cucumber.java.en.*;
+
+public class StepDefination {
+
+	@Given("Add Place Payload with API Call")
+	public void add_place_payload_with_api_call() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user calls AddPlaceAPI with POST http request")
+	public void user_calls_add_place_api_with_post_http_request() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the API call got success with status code {int}")
+	public void the_api_call_got_success_with_status_code(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("{string} in response body is {string}")
+	public void in_response_body_is(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
